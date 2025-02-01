1093d534d78eed1cbee18b8aa798e7f2a8d2bd3d
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2025-02-01 23:14:45 +0300

    create a signIn and signOut

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import GitHub from "@auth/core/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import {db} from '@/db';
-import { use } from "react";
 
 const GITHUB_CLIENT_ID =process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET=process.env.GITHUB_CLIENT_SECRET;
